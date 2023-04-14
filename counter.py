fcffcba0fc460f66f4e719fddc3b168475c0a5b4
Teacher <zej956@gmail.com>
2023-04-14 17:38:34 +0800

    new method to set student propfile

--- counter.py
+++ counter.py
@@ -4,3 +4,9 @@ class Urian:
     name: str
     age: str
     course: str
+
+    def nominate(self, student: "Urian") -> None:
+        self._set_student_profile(student)
+
+    def _set_student_profile(self, student: "Urian") -> None:
+        pass
